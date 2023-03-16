dafe73b68d29e9cf82147d54a8ad86e18adee60f
eloipelmon <101187279+eloipelmon@users.noreply.github.com>
2023-03-16 20:26:53 +0100

    ApiHTTP

---
+++ src/apihttp/ConsumirApiHttp.java
@@ -0,0 +1,38 @@
+package apihttp;       
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+public class ConsumirApiHttp {
+
+
+    public static void main(String[] args) {
+
+        try{
+            
+            URL url = new URL("http://localhost:8080/products");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+
+            int responseCode = conn.getResponseCode();
+            if(responseCode != 200){
+                throw new RuntimeException("Error "+ responseCode);
+            } else{
+                StringBuilder informationString = new StringBuilder();
+                Scanner scanner = new Scanner(url.openStream());
+
+                while(scanner.hasNext()){
+                    informationString.append(scanner.nextLine());
+                }
+                scanner.close();
+
+                System.out.println(informationString);
+            }
+
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
